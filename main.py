37768f382cba3e5f1a2c88be0365c63c2e2fb3c8
lukejwamble <wambleson3@gmail.com>
2024-02-09 11:51:24 -0600

    push 1
    completed and ran test via terminal, all passed there, running in git to test there

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    backwards = word[::-1]
+
+    if word == backwards and word != "":
+        return True
+    else:
+        return False
 
 #YOUR CODE GOES HERE
+if __name__ == "__main__":
+    print(palindrome(input().lower().replace(" ", "")))
